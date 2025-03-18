31d21f99e430e65823e8db07c27834aa4fdf7a27
qiuyuting <qiuyuting@zjnu.edu.cn>
2025-03-18 18:13:02 +0800

    限制用户名只能由英文字母、数字、下划线和短横组成
    - 同时限制用户名和密码的长度；
    - 移除 ajv 和 ajv-errors 库；
    - 改用正则表达式进行校验用户名和密码；

--- routers/createUser.js
+++ routers/createUser.js
@@ -1,64 +1,20 @@
 import { db } from '../db.js';
-import Ajv from 'ajv';
-import addErrors from 'ajv-errors';
 import bcrypt from 'bcryptjs';
 
-const ajv = new Ajv({ allErrors: true, useDefaults: true, coerceTypes: true });
-
-addErrors(ajv);
-
-const schema = {
-  type: 'object',
-  properties: {
-    name: { 
-      type: 'string', 
-      minLength: 6, 
-      maxLength: 64,
-      errorMessage: {
-        type: '用户名必须是字符串！',
-        minLength: '用户名不能少于6个字符！',
-        maxLength: '用户名不能超过64个字符！',
-      },
-    },
-    password: { 
-      type: 'string', 
-      minLength: 8, 
-      maxLength: 32,
-      errorMessage: {
-        type: '密码必须是字符串！',
-        minLength: '密码不能少于8个字符！',
-        maxLength: '密码不能超过32个字符！',
-      },
-    },
-  },
-  required: ['name', 'password'], 
-  additionalProperties: false,
-  errorMessage: {
-    required: {
-      name: '请输入用户名！',
-      password: '请输入密码！',
-    },
-    additionalProperties: '不允许额外的字段！',
-  }, 
-};
-
-const validate = ajv.compile(schema);
-
-// TODO: 限制用户名只能由字母和数字组成。
-
 export default async (ctx, next) => {
-  try {
-    const valid = validate(ctx.request.body);
-    if (!valid) {
-      throw new Error(validate.errors[0].message);
-    }
-  } catch (err) {
+  const { name, password } = ctx.request.body;
+
+  if (!name || !/^[a-zA-Z0-9_-]{2,15}$/.test(name)) {
     ctx.status = 400;
-    ctx.body = { msg: err.message };
+    ctx.body = { msg: '用户名只能由英文字母、数字、下划线和短横组成，且长度应为 2-15 个字符' };
     return;
   }
 
-  const { name, password } = ctx.request.body;
+  if (!password || password.length < 8 || password.length > 72) {
+    ctx.status = 400;
+    ctx.body = { msg: '密码长度应为 8-72 个字符' };
+    return;
+  }
 
   const collection = db.collection('users');
   const user = await collection.findOne({ name });
@@ -76,5 +32,4 @@ export default async (ctx, next) => {
     ctx.status = 201;
     ctx.body = {msg: '创建成功！'};
   }
-
 }
